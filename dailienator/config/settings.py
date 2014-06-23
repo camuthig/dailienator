42c3e05182b3d4c89e8f38692b9ac844865f54c9
CHris Muthig <camuthig@gmail.com>
2014-06-22 18:23:43 -0700

    #1 Made a copy of AESField and added reqs/settings for storing catertrax credentials.

--- dailienator/config/settings.py
+++ dailienator/config/settings.py
@@ -29,6 +29,10 @@ ALLOWED_HOSTS = []
 #Override the authentication user with my customer model
 AUTH_USER_MODEL = 'daily.AccountUser'
 
+# Keys used by AESFields
+AES_KEYS= {
+	'catertrax_key': os.path.join(BASE_DIR, 'config', 'catertrax.key'),
+}
 
 # Application definition
 
@@ -43,10 +47,12 @@ CORE_APPS = (
 
 THIRD_PARTY_APPS = (
 	'south',
+	'm2secret',
 )
 
 INTERNAL_APPS = (
 	'dailienator.daily',
+	'dailienator.common.aesfield',
 )
 
 INSTALLED_APPS = CORE_APPS + THIRD_PARTY_APPS + INTERNAL_APPS
