c2710e1e1a3a12ac8587bcb0f10aba8f669f21ef
Chris Muthig <chris.muthig@equifax.com>
2015-01-15 22:09:42 -0800

    Update dailienator wsgi file to work (hopefully) with OpenShift

--- dailienator/wsgi.py
+++ dailienator/wsgi.py
@@ -10,5 +10,14 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dailienator.config.settings")
 
+
+# Set up the virtualenv stuff on OpenShift
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
