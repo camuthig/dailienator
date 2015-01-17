f1aa0b52a677acca07e70c2a8174099bb957c6ba
Chris Muthig <chris.muthig@equifax.com>
2015-01-17 09:47:04 -0800

    Clean up Openshift settings around WSGI.

--- dailienator/wsgi.py
+++ dailienator/wsgi.py
@@ -11,13 +11,14 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dailienator.config.settings")
 
 
-# Set up the virtualenv stuff on OpenShift
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
+if os.environ.has_key('OPENSHIFT_REPO_DIR'):
+    # Set up the virtualenv stuff on OpenShift
+    virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+    virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+    try:
+        execfile(virtualenv, dict(__file__=virtualenv))
+    except IOError:
+        pass
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='Dailienator',
       version='0.1',
-      description='Catertrax ',
+      description='Catertrax daily handling for Sodexo accounts. This tools provides easy to use Excel sheets based on CaterTrax schedule for a given day.',
       author='Chris Muthig',
       author_email='camuthig@gmail.com',
       url='http://dailienator-kringle.rhcloud.com/',
