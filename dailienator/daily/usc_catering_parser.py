fb78597d5ddb22b73e6af97e968e4ea915b7181b
Chris Muthig <camuthig@gmail.com>
2015-04-09 21:35:25 -0700

    Remove RowData from USC parser. Remove floor from USC location string.

--- dailienator/daily/usc_catering_parser.py
+++ dailienator/daily/usc_catering_parser.py
@@ -7,11 +7,6 @@ from basic_parser import BasicParser
 
 logger = logging.getLogger(__name__)
 
-class RowData(object):
-    """A class representing the data for a single row in the target Excel
-    sheet. This is empty because it is just data attributes"""
-    pass
-
 class Parser(BasicParser):
     building_key = 'Building'
     room_key     = 'Room #'
@@ -21,17 +16,14 @@ class Parser(BasicParser):
 
     def buildLocationString(self, row):
         """
-            Utility method to build the hyphen delimited location string.
+            USC doesn't include a floor in their description of
+            the location
         """
         locationString = ""
         if hasattr(row, 'building'):
                 locationString = row.building + " - "
         else:
             locationString = "-"
-        if hasattr(row, 'floor'):
-            locationString = locationString + row.floor + " - "
-        else:
-            locationString = locationString + " - "
         if hasattr(row, 'room'):
             locationString = locationString + row.room
         else:
